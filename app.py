0745dd885920431620cf0bd76c0900a9fba56175
a.lazev@transdata.no <a.lazev@transdata.no>
2023-04-20 03:26:30 +0300

    register def

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import config
 from service import SocialNetworkScraper
 
 if __name__ == '__main__':
     service = SocialNetworkScraper()
-    title = 'This post created by selenium'
-    content = 'Selenium is an open source automation testing tool that supports a number of scripting languages' \
-              ' like Python, C#, Java, Perl, Ruby, JavaScript, etc. depending on the application to be tested.' \
-              ' One can choose the script accordingly'
-    service.create_post(title,content)
-    print('Done')
+    title = 'Post created after selenium register user'
+    content = 'Beautiful Soup is a library that makes it easy to scrape information from web pages. ' \
+              'It sits atop an HTML or XML parser, providing Pythonic idioms for iterating, searching, and modifying' \
+              ' the parse tree.'
+    username = config.SOCIAL_NETWORK_REGISTER_USER
+    password = config.SOCIAL_NETWORK_REGISTER_PASSWORD
+    email = config.SOCIAL_NETWORK_REGISTER_EMAIL
+    service.social_network_register(username,password,email)
+    service.create_post(title, content)
+    # print('Done')
